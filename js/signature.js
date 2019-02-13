b0b88a17d70bd8063595966fd218341ee4533269
Meiyo40 <loic.demesmay@gmail.com>
2019-02-13 16:40:16 +0100

    adding signature and sessionStorage system with user reservation info + chrono

--- js/signature.js
+++ js/signature.js
@@ -1,13 +1,43 @@
-//CANVAS DISPLAY
+//CANVAS DISPLAY + BUTTON BEHAVIOR
 let reserveBtn = document.getElementById('reserve-btn');
 let canvasForm = document.getElementById('signature-form');
 let formPad = document.getElementById('UserForm');
+let cancelBtn = document.getElementById('cancel');
+let saveBtn = document.getElementById('save');
 
 reserveBtn.onclick = function (){
-    canvasForm.setAttribute('style', 'display: block'); 
-    reserveBtn.setAttribute('style', 'display: none');
-    formPad.setAttribute('style', 'height: 620px');
+    let availableBike = parseInt(document.getElementById('velo').placeholder);
+    if( availableBike > 0){
+        canvasForm.setAttribute('style', 'display: block'); 
+        reserveBtn.setAttribute('style', 'display: none');
+        formPad.setAttribute('style', 'height: auto');
+    }
+    else{
+        alert('Cette station ne dispose de pas velo disponible, merci de choisir une autre station');
+    }
+    
 };
+
+cancelBtn.onclick = function(){
+    canvasForm.setAttribute('style', 'display: none');
+    reserveBtn.setAttribute('style', 'display: block margin: 0 auto');
+    formPad.setAttribute('style', 'height: 480px');
+}
+
+saveBtn.onclick = function(){
+    
+    user.savedHour = ((Date.now() % (24 * 60 * 60 * 1000)) - (new Date().getTimezoneOffset() * 60 * 1000)); // on sauvegarde l'heure de l'enregistrement en format milliseconde pour la comparaison du chrono
+    user.time = timer; //on lance le timer de 20min definit dans reservation.js
+    let img = document.getElementById('signature-pad');
+    let urlIMG = img.toDataURL();
+    let resaStation = document.getElementById('name').placeholder;
+    
+    sessionStorage.setItem('signature', urlIMG);
+    sessionStorage.setItem('station', resaStation);
+    userInfo(20, 00); //On edit la partie pour les informations
+}
+
+
 
 $(document).ready(function() {
 	
@@ -15,10 +45,15 @@ $(document).ready(function() {
 	let color = "#000";
 	let painting = false;
 	let started = false;
-	let width_brush = 5;
+	let width_brush = 2;
 	let canvas = $("#signature-pad");
 	let cursorX, cursorY;
 	let context = canvas[0].getContext('2d');
+    let paintWindow = document.querySelector('#canvas-container');
+    let windowStyle = getComputedStyle(paintWindow);
+    //Ajuste la taille du canvas a son container, JS oblige pour éviter des soucis avec les coords de la souris
+    context.canvas.width = parseInt(windowStyle.getPropertyValue('width'));
+    context.canvas.height = parseInt(windowStyle.getPropertyValue('height'));
 	
     
     
@@ -28,6 +63,7 @@ $(document).ready(function() {
 		// Coordonnées de la souris :
 		cursorX = (e.pageX - this.offsetLeft);
 		cursorY = (e.pageY - this.offsetTop);
+        paint();
 	});
 	
 	// Relachement du Click arrête de dessiner :
@@ -41,14 +77,17 @@ $(document).ready(function() {
 		// Si je suis en train de dessiner (click souris enfoncé) :
 		if (painting) {
 			// Set Coordonnées de la souris :
-			cursorX = (e.pageX - this.offsetLeft) - 10; // 10 = décalage du curseur
-			cursorY = (e.pageY - this.offsetTop) - 10;
+			cursorX = (e.pageX - this.offsetLeft); // 10 = décalage du curseur
+			cursorY = (e.pageY - this.offsetTop);
 			
 			// Dessine une ligne :
 			drawLine();
 		}
 	});
-	
+	function paint (){
+        context.lineTo(cursorX, cursorY);
+        context.stroke();
+    }
 	// Fonction qui dessine une ligne :
 	function drawLine() {
 		// Si c'est le début, j'initialise
@@ -62,7 +101,7 @@ $(document).ready(function() {
 		else {
 			context.lineTo(cursorX, cursorY);
 			context.strokeStyle = color;
-			context.lineWidth = 3;
+			context.lineWidth = width_brush;
 			context.stroke();
 		}
 	}
@@ -77,9 +116,9 @@ $(document).ready(function() {
 	});
 	
 	// Bouton Save :
-	$("#save").click(function() {
-		let canvas_tmp = document.getElementById("canvas");
-		window.location = canvas_tmp.toDataURL("image/png");
-	});
+//	$("#save").click(function() {
+//		let canvas_tmp = document.getElementById("canvas");
+//		window.location = canvas_tmp.toDataURL("image/png");
+//	});
 	
 });
