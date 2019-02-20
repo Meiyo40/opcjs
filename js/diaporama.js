d1a3de2f66df7dc336c6a6300897a13ca858fca3
Meiyo40 <loic.demesmay@gmail.com>
2019-02-20 14:50:01 +0100

    change carousel working, add fadeIn

--- js/diaporama.js
+++ js/diaporama.js
@@ -1,14 +1,12 @@
 "use strict";
 
-let slides = ['assets/diapo/etapes.png',
-              'assets/diapo/diapo1.png',
+let slides = ['assets/diapo/diapo1.png',
               'assets/diapo/diapo2.png',
               'assets/diapo/diapo3.png', 
               'assets/diapo/diapo4.png'];
-let descriptions = ["Suivez rapidement quelques étapes pour réserver votre velib",
-                    "<strong>Tutoriel etape 1:</strong> Choisissez une des stations disponible",
-                    "<strong>Tutoriel etape 2:</strong> Saisissez votre <strong>Nom</strong> et votre <strong>Prénom</strong>, puis cliquez sur 'Réservation'.",
-                    "<strong>Tutoriel étape 3:</strong> Signez le formulaire, si tout est en ordre, vous verrez une fenetre confirmant la réservation apparaître, attention, la réservation n'est valable que 20 min.",
+let descriptions = ["<strong>Tutoriel étape 1/3:</strong> Choisissez une des stations disponible",
+                    "<strong>Tutoriel étape 2/3:</strong> Saisissez votre <strong>Nom</strong> et votre <strong>Prénom</strong>, puis cliquez sur 'Réservation'.",
+                    "<strong>Tutoriel étape 3/3:</strong> Signez le formulaire, si tout est en ordre, vous verrez une fenetre confirmant la réservation apparaître, attention, la réservation n'est valable que 20 min.",
                    "<strong>Profitez maintenant de votre réservation !</strong>"];
 let slider = new Slider(slides ,descriptions);
 
@@ -22,7 +20,9 @@ function Slider (slides, descriptions){
     this.descriptions = descriptions;
     this.currentSlide = 0;
 }
-
+let test = setInterval(function(){
+    console.log (document.getElementById('slide').getAttribute('opacity'));
+}, 100);
 
 function add(){
     if(slider.currentSlide < (slider.slides.length-1)){
@@ -44,27 +44,38 @@ function less(){
     Carousel();
 }
 
+
 function Carousel(active = true, position = 0){
-    let carousel = document.getElementById('slide');
+    let slide = document.getElementById('slide');
+    let carouselContainer = document.getElementById('carousel');
+    let figure = document.getElementById('slide_container');
     let slideWindow = document.getElementById('diaporama');
     let description = document.getElementById('description');
     let width = slideWindow.offsetWidth;
     let pos = position;
-    carousel.src = slider.slides[slider.currentSlide];
+    slide.src = slider.slides[slider.currentSlide];
+    figure.style.left = '0';
+    slide.classList.add('animate');
     description.innerHTML = slider.descriptions[slider.currentSlide];
-    carousel.classList.add('animate');
-    carousel.style.marginLeft = pos;
+    slide.style.marginLeft = pos;
     clearInterval(autoTimer);
+    
+    //ADAPT CONTAINER SIZE TO FIGURE SIZE
+    let height = figure.offsetHeight;
+    carouselContainer.style.height = height + 30 + 'px';
+    
+ 
+    
     if(active){
         timerAnimation = setInterval(moveRight, 5);
         function moveRight(){
             if(pos > slideWindow.offsetWidth){
                 clearInterval(timerAnimation);
                 slider.currentSlide++;
-                carousel.setAttribute('style', 'opacity: 0');
                 if(slider.currentSlide > (slider.slides.length - 1)){
                     slider.currentSlide = 0;
                 }
+                slide.classList.remove('animate');
                 Carousel();
             }
             else if(pos == 0){
@@ -75,7 +86,7 @@ function Carousel(active = true, position = 0){
             }
             else{
                 pos+=4;
-                carousel.style.marginLeft = pos + 'px';
+                figure.style.left = pos + 'px';
             }
         }
     }
