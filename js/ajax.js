2f52c2342f2cd408e068d97ded2a7723d52cc477
Meiyo40 <loic.demesmay@gmail.com>
2019-03-22 13:35:54 +0100

    delete useless stuff, bad push

--- js/ajax.js
+++ js/ajax.js
@@ -1,39 +1,3 @@
-function getXMLHttpRequest() {
-    let xhr = null;
-    if (window.XMLHttpRequest || window.ActiveXObject) {
-        if (window.ActiveXObject) {
-            try {
-                xhr = new ActiveXObject("Msxm12.XMLHTTP");
-            } catch (e) {
-                xhr = new ActiveXObject("Microsoft.XMLHTTP");
-            }
-        } else {
-            xhr = new XMLHttpRequest();
-        }
-    } else {
-        alert("Votre navigateur ne supporte pas l'objet XMLHttpRequest");
-        return null;
-    }
-    return xhr;
-}
-
-function ajaxGet(url, Map) {
-    var oXhr = getXMLHttpRequest();
-
-    oXhr.onerror = function (data) {
-        console.log('Erreur... ');
-    };
-
-    oXhr.open("GET", url, true);
-    oXhr.responseType = 'json';
-    oXhr.send();
-
-    oXhr.onload = function () {
-        let setStation = new Station(this.response[0]);
-        setStation.setStations(this.response, Map);
-    }
-}
-
 function Ajax(url) {
     this.url = url;
     
